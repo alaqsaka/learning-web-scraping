6b613660cf25fa38447b81b6ea540acdc235ab6c
Al-Aqsa Krisnaya Abidin <40936981+alaqsaka@users.noreply.github.com>
2020-11-26 21:00:47 +0700

    Source code for this project.

---
+++ web_scrap.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+# Web Scraping Real Website
+print('Put some skill that your are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
+
+
+
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+#print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+for index, job in enumerate(jobs):
+    published_date = job.find('span', class_ = 'sim-posted').span.text
+    if 'few' in published_date:
+        company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_ = 'srp-skills').text.replace(' ','')
+        more_info = job.header.h2.a['href']
+        if unfamiliar_skill not in skills:
+            with open(f'posts/{index}.txt', 'w') as f:
+                f.write(f"More Info: {more_info} \n")
+                f.write(f"Company Name: {company_name.strip()} \n")
+                f.write(f"Required Skills: {skills.strip()} \n")
+            print(f'File saved: {index}')
+'''
+if __name == '__main__':
+    while True: 
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} seconds...')
+        time.sleep(time_wait * 60)
+'''
